3b99c40203f024c0823fa147167cfa448105bd18
P-20230625HCKWI\Administrator <347863733@qq.com>
2025-04-23 20:34:17 +0800

    luban提交下配置

--- MiniTemplate/gen_client_lua.bat
+++ MiniTemplate/gen_client_lua.bat
@@ -3,8 +3,8 @@ set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
 set CONF_ROOT=.
 
 dotnet %LUBAN_DLL% ^
-    -t all ^
-    -d json ^
+    -t client ^
+    -d lua ^
     --conf %CONF_ROOT%\luban.conf ^
     -x outputDataDir=output
 
---
+++ MiniTemplate/output/tbreward.lua
@@ -0,0 +1,6 @@
+return
+{
+[1001] = {id=1001,name="测试道具1",desc="描述1111",count=1,color=1,flag=1,item2Param={id=1,type="武器",},reward={1,2,3,4,5,6,},reward2={{id=1,num=3,},{id=2,num=1,},{id=2,num=3333,},},reward3={{id=11111,num=3,},{id=1,num=22,},},},
+[1002] = {id=1002,name="测试道具2",desc="描述1112",count=2,color=3,flag=3,item2Param={id=2,type="武器",},reward={3,2,3,4,5,6,},reward2={{id=1,num=2,},{id=2,num=3,},},reward3={{id=1777,num=2,},{id=3,num=2,},},},
+[1003] = {id=1003,name="测试道具3",desc="描述1113",count=3,color=3,flag=3,item2Param={id=3,type="111111111111",},reward={3,2,3,4,5,7,},reward2={{id=1,num=2,},{id=2,num=4,},},reward3={{id=1777,num=3,},{id=3,num=3,},},},
+}
